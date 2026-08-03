194a617d88720e002db09c1a790ba4358d1f0e45
Shohanul Islam <shohanulislam19892@gmail.com>
2026-08-03 20:11:01 +0600

    Update DailyCheckInAdmin with what_went_well

--- users/admin.py
+++ users/admin.py
@@ -8,9 +8,9 @@ from django.utils.html import format_html
 
 @admin.register(DailyCheckIn)
 class DailyCheckInAdmin(admin.ModelAdmin):
-    list_display = ('user', 'hearing_status', 'checkin_date', 'created_at')
+    list_display = ('user', 'hearing_status', 'what_went_well', 'checkin_date', 'created_at')
     list_filter = ('hearing_status', 'checkin_date')
-    search_fields = ('user__email', 'user__name')
+    search_fields = ('user__email', 'user__name', 'what_went_well')
     readonly_fields = ('created_at',)
 
 @admin.register(UserOnboarding)
