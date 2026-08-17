917449593d6523646d680098af7d95ced41a44f6
Shohanul Islam <shohanulislam19892@gmail.com>
2026-08-17 16:49:45 +0600

    feat(learn): add URL routing for Welcome Tutorial API

--- learn/urls.py
+++ learn/urls.py
@@ -1,9 +1,13 @@
 from django.urls import path
-from .views import TodayLessonView
+from .views import TodayLessonView, WelcomeTutorialView
 
 app_name = 'learn'
 
 urlpatterns = [
+    # Welcome Tutorial Video API
+    path('welcome-tutorial/', WelcomeTutorialView.as_view(), name='welcome-tutorial'),
+    path('welcome/', WelcomeTutorialView.as_view(), name='welcome-tutorial-alias'),
+
     # API for Today's Lesson (video & audio)
     path('', TodayLessonView.as_view(), name='learn-index'),
     path('today/', TodayLessonView.as_view(), name='today-lesson'),
