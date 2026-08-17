a759718134c5cd43777ecd27c3b00b8c2dbf7683
Shohanul Islam <shohanulislam19892@gmail.com>
2026-08-17 22:11:46 +0600

    feat(skills_strategies): create AppConfig for Skills & Strategies feature

---
+++ skills_strategies/apps.py
@@ -0,0 +1,7 @@
+from django.apps import AppConfig
+
+
+class SkillsStrategiesConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'skills_strategies'
+    verbose_name = 'Skills & Strategies'
