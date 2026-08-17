795df02f565da3b3ac16a9e7de36e6815381f415
Shohanul Islam <shohanulislam19892@gmail.com>
2026-08-17 15:46:34 +0600

    feat(weekly_tutorials): add app configuration

---
+++ weekly_tutorials/apps.py
@@ -0,0 +1,7 @@
+from django.apps import AppConfig
+
+
+class WeeklyTutorialsConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'weekly_tutorials'
+    verbose_name = 'Weekly Tutorials & Tips'
