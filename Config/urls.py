9a6f532be569f828bc689b11adf670dc1905d9ff
Shohanul Islam <shohanulislam19892@gmail.com>
2026-08-17 17:46:32 +0600

    config: route api/what-normal/ in main project URL configuration

--- Config/urls.py
+++ Config/urls.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from django.urls import path , include
+from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -8,8 +8,5 @@ urlpatterns = [
     path('api/weekly-tutorials/', include('weekly_tutorials.urls', namespace='weekly_tutorials')),
     path('api/weekly-tips/', include(('weekly_tutorials.urls', 'weekly_tips'), namespace='weekly_tips')),
     path('api/learn/', include(('learn.urls', 'learn'), namespace='learn')),
+    path('api/what-normal/', include(('what_normal.urls', 'what_normal'), namespace='what_normal')),
 ]
-
-
-
-
