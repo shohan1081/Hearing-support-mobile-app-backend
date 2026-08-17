398d169149a7d7bc35691daf99494e1bbc3d3812
Shohanul Islam <shohanulislam19892@gmail.com>
2026-08-17 22:12:10 +0600

    feat(skills_strategies): register EverydayListeningTip in Unfold admin panel

---
+++ skills_strategies/admin.py
@@ -0,0 +1,24 @@
+from django.contrib import admin
+from unfold.admin import ModelAdmin
+from .models import EverydayListeningTip
+
+
+@admin.register(EverydayListeningTip)
+class EverydayListeningTipAdmin(ModelAdmin):
+    list_display = ('title', 'slug', 'order', 'duration_seconds', 'is_active', 'updated_at')
+    list_editable = ('order', 'is_active')
+    list_filter = ('is_active',)
+    search_fields = ('title', 'slug', 'subtitle', 'description')
+    prepopulated_fields = {'slug': ('title',)}
+    ordering = ('order', 'created_at')
+    fieldsets = (
+        (None, {
+            'fields': ('title', 'slug', 'subtitle', 'order', 'is_active')
+        }),
+        ('Tip Description & Guidance', {
+            'fields': ('description',)
+        }),
+        ('Upload Audio & Thumbnail', {
+            'fields': ('audio_file', 'thumbnail', 'duration_seconds')
+        }),
+    )
