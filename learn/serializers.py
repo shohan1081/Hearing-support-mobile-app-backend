ba4fa999cedaac5fa6c925a540c6bfb6e97c80a9
Shohanul Islam <shohanulislam19892@gmail.com>
2026-08-17 15:53:57 +0600

    feat(learn): add DRF serializers for daily lesson API

---
+++ learn/serializers.py
@@ -0,0 +1,64 @@
+from rest_framework import serializers
+from .models import DailyLesson, UserLessonProgress
+
+
+class DailyLessonSerializer(serializers.ModelSerializer):
+    """
+    Serializer for Today's Lesson (video and audio content)
+    """
+    video_url = serializers.SerializerMethodField()
+    audio_url = serializers.SerializerMethodField()
+    has_video = serializers.SerializerMethodField()
+    has_audio = serializers.SerializerMethodField()
+
+    class Meta:
+        model = DailyLesson
+        fields = [
+            'id',
+            'day_number',
+            'title',
+            'subtitle',
+            'description',
+            'video_url',
+            'audio_url',
+            'has_video',
+            'has_audio',
+            'thumbnail',
+            'duration_seconds',
+            'key_takeaways',
+            'created_at',
+            'updated_at',
+        ]
+
+    def get_video_url(self, obj):
+        request = self.context.get('request')
+        return obj.get_video_stream_url(request=request)
+
+    def get_audio_url(self, obj):
+        request = self.context.get('request')
+        return obj.get_audio_stream_url(request=request)
+
+    def get_has_video(self, obj):
+        return bool(obj.video_file or obj.video_url)
+
+    def get_has_audio(self, obj):
+        return bool(obj.audio_file or obj.audio_url)
+
+
+class UserLessonProgressSerializer(serializers.ModelSerializer):
+    """
+    Serializer for user lesson progress status summary
+    """
+    current_day = serializers.SerializerMethodField()
+
+    class Meta:
+        model = UserLessonProgress
+        fields = [
+            'start_date',
+            'current_day',
+            'created_at',
+            'updated_at',
+        ]
+
+    def get_current_day(self, obj):
+        return obj.get_current_day()
