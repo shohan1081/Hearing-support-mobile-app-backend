3b321bc10f48d23ba89385270a43a2fdd78ffe5a
Shohanul Islam <shohanulislam19892@gmail.com>
2026-08-17 15:55:00 +0600

    config: register URL routes for weekly_tutorials and learn apps

--- Config/urls.py
+++ Config/urls.py
@@ -5,4 +5,11 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/users/', include('users.urls')),
     path('api/legal/', include('legal_pages.urls')),
+    path('api/weekly-tutorials/', include('weekly_tutorials.urls', namespace='weekly_tutorials')),
+    path('api/weekly-tips/', include(('weekly_tutorials.urls', 'weekly_tips'), namespace='weekly_tips')),
+    path('api/learn/', include(('learn.urls', 'learn'), namespace='learn')),
 ]
+
+
+
+
