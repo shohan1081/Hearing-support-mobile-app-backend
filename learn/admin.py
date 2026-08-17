09ba602cea71b4def698eb6dbc0421e5aa33c916
Shohanul Islam <shohanulislam19892@gmail.com>
2026-08-17 15:53:49 +0600

    feat(learn): add Unfold admin panel configuration

---
+++ learn/admin.py
@@ -0,0 +1,42 @@
+from django.contrib import admin
+from unfold.admin import ModelAdmin
+from .models import DailyLesson, UserLessonProgress
+
+
+@admin.register(DailyLesson)
+class DailyLessonAdmin(ModelAdmin):
+    list_display = ('day_number', 'title', 'subtitle', 'is_active', 'updated_at')
+    list_editable = ('is_active',)
+    list_filter = ('is_active',)
+    search_fields = ('title', 'subtitle', 'description')
+    ordering = ('day_number',)
+    fieldsets = (
+        (None, {
+            'fields': ('day_number', 'title', 'subtitle', 'is_active')
+        }),
+        ('Lesson Text & Content', {
+            'fields': ('description', 'key_takeaways')
+        }),
+        ('Media Content (Video & Audio)', {
+            'fields': (
+                'video_file',
+                'video_url',
+                'audio_file',
+                'audio_url',
+                'thumbnail',
+                'duration_seconds',
+            )
+        }),
+    )
+
+
+@admin.register(UserLessonProgress)
+class UserLessonProgressAdmin(ModelAdmin):
+    list_display = ('user', 'get_current_day', 'start_date', 'completed_days', 'updated_at')
+    search_fields = ('user__email', 'user__name')
+    list_filter = ('start_date',)
+    ordering = ('-updated_at',)
+
+    def get_current_day(self, obj):
+        return f"Day {obj.get_current_day()}"
+    get_current_day.short_description = 'Current Day'
