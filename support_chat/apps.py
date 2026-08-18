f42725b62cb0e0d00ba0c05b2f8d02f920f62fdd
Shohanul Islam <shohanulislam19892@gmail.com>
2026-08-18 19:33:09 +0600

    feat(support_chat): create AppConfig for Care Team Support Chat feature

---
+++ support_chat/apps.py
@@ -0,0 +1,7 @@
+from django.apps import AppConfig
+
+
+class SupportChatConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'support_chat'
+    verbose_name = 'Care Team Support Chat'
