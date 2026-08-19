8dcdcce6ec50aac3d73e404e1fd01e7a3eca7b72
Shohanul Islam <shohanulislam19892@gmail.com>
2026-08-19 22:33:14 +0600

    fix(config): update ALLOWED_HOSTS to accept wildcard or environment variables for production server

--- Config/settings.py
+++ Config/settings.py
@@ -12,12 +12,14 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-ah+qh6%9#=jnm-vw(y9=u3wf5#(30$w%=7#g8xz*r^m&+&l^4z'
+SECRET_KEY = config('SECRET_KEY', default='django-insecure-ah+qh6%9#=jnm-vw(y9=u3wf5#(30$w%=7#g8xz*r^m&+&l^4z')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = config('DEBUG', default=True, cast=bool)
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = config('ALLOWED_HOSTS', default='*', cast=lambda v: [s.strip() for s in v.split(',') if s.strip()])
+if '*' in ALLOWED_HOSTS or not ALLOWED_HOSTS:
+    ALLOWED_HOSTS = ['*']
 
 
 # Application definition
