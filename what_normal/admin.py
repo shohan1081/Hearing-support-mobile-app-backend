42883c36d98e278fbc9b206cabe54ac8560ed368
Shohanul Islam <shohanulislam19892@gmail.com>
2026-09-23 11:26:01 +0600

    feat(what_normal): enable direct s3 video upload in what-normal video admin

--- what_normal/admin.py
+++ what_normal/admin.py
@@ -1,12 +1,13 @@
 from django.contrib import admin
 from django.utils.translation import gettext_lazy as _
+from users.direct_upload import DirectVideoUploadAdminMixin
 from unfold.admin import ModelAdmin
 from users.video_utils import get_video_status_badge, render_video_preview_html
 from .models import WhatNormalVideo, WhatNormalAudio
 
 
 @admin.register(WhatNormalVideo)
-class WhatNormalVideoAdmin(ModelAdmin):
+class WhatNormalVideoAdmin(DirectVideoUploadAdminMixin, ModelAdmin):
     list_display = ('title', 'order', 'subtitle', 'video_status', 'is_active', 'updated_at')
     list_editable = ('order', 'is_active')
     list_filter = ('is_active',)
